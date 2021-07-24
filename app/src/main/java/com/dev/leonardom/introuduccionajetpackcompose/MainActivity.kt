26019c88e1fd8b3614327e006d2afc282c9dc22c
LeonardoMSycnos <leonardom@sycnos.com>
2021-07-23 22:59:26 -0500

    Componentes Básicos en Compose

--- app/src/main/java/com/dev/leonardom/introuduccionajetpackcompose/MainActivity.kt
+++ app/src/main/java/com/dev/leonardom/introuduccionajetpackcompose/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -19,11 +20,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
@@ -40,38 +47,63 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SimpleComposable() {
-    var num by remember { mutableStateOf(0) }
 
-    Column {
-        Text(text = "Conteo : $num")
+    var textFieldValue by remember { mutableStateOf("") }
+    var textValue by remember { mutableStateOf("Título de la Aplicación") }
 
-        Divider(thickness = 20.dp, color = Color.White)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            painter = painterResource(id = R.drawable.bg),
+            contentDescription = "Imagen de fondo",
+            contentScale = ContentScale.FillWidth
+        )
 
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            Counter(
-                "Aumentar Conteo",
-                updateCount = { num++ }
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = textValue,
+            style = TextStyle(
+                color = Color.Green,
+                fontSize = 28.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Black,
+                textAlign = TextAlign.Center
             )
+        )
 
-            Counter(
-                "Disminuir Conteo",
-                updateCount = { num-- }
-            )
+        TextField(
+            value = textFieldValue,
+            onValueChange = { nuevoTexto ->
+                textFieldValue = nuevoTexto
+            },
+            label = {
+                Text(text = "Inserte texto aquí")
+            },
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(id = R.drawable.ic_android),
+                    contentDescription = "Icono"
+                )
+            }
+        )
+
+        Button(
+            onClick = {
+                textValue = textFieldValue
+            }
+        ) {
+            Text("Acaptar")
         }
     }
 }
 
-@Composable
-fun Counter(
-    btnTitulo: String,
-    updateCount: () -> Unit
-) {
-    Button(
-        onClick = { updateCount() }
-    ) {
-        Text(btnTitulo)
-    }
-}
 
 @Preview(showBackground = true, name = "NuevoNombre", showSystemUi = true, device = Devices.DEFAULT)
 @Composable
